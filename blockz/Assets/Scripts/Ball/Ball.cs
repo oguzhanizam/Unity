8adcef35a2c442ae605c4b8839a86445553e70f2
oguzhanizam <o.nizam@outlook.com.tr>
2019-12-19 15:00:15 +0300

    blokz

---
+++ blockz/Assets/Scripts/Ball/Ball.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    // Parametreler
+    Vector2 paddleToBallVector;
+    float xPush = 2f;
+    float yPush = 10f;
+    float unitWidth = 16f;
+    float unitHeight = 10f;
+
+
+    // Eşlemeler
+    [SerializeField] Paddle paddle1;
+    [SerializeField] AudioClip[] ballCollisionSounds;
+
+    // cached component ref
+    Rigidbody2D rb;
+    AudioSource audioSource;
+    Block block;
+
+    // Durumlar
+    bool hasStarted = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        paddleToBallVector = transform.position - paddle1.transform.position;
+        rb = GetComponent<Rigidbody2D>(); // cache ref component tanımlama
+        audioSource = GetComponent<AudioSource>(); // cache ref component tanımlama
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!hasStarted)
+        {
+            LockBallToPaddle();
+            LaunchOnClick();
+        }
+    }
+
+    private void LaunchOnClick()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            hasStarted = true;
+            //rb.velocity = new Vector2(mouseWidthInScreenUnits, mouseHeightInScreenUnits);
+            rb.velocity = new Vector2(xPush, yPush);
+        }
+    }
+
+    private void LockBallToPaddle()
+    {
+        Vector2 paddlePos = new Vector2(paddle1.transform.position.x, paddle1.transform.position.y);
+        transform.position = paddlePos + paddleToBallVector;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (hasStarted)
+        {
+            //GetComponent<AudioSource>().Play();
+            AudioClip clip = ballCollisionSounds[UnityEngine.Random.Range(0, ballCollisionSounds.Length)];
+            audioSource.PlayOneShot(clip);
+            //AudioSource.PlayClipAtPoint(clip, Camera.main.transform.position);
+        }
+    }
+}
---
+++ blockz/Assets/Scripts/Paddle/Paddle.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    // Parametreler
+    [SerializeField] float unitSize = 16f;
+    [SerializeField] float minX = 1.17f;
+    [SerializeField] float maxX = 14.83f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mousePosInScreenUnits = Input.mousePosition.x / Screen.width * unitSize;  // mouse konumunu screen unit formatına çevirdi
+        Vector2 paddlePos = new Vector2(transform.position.x, transform.position.y); // paddle'ın yeni konumunu tanımladı
+        paddlePos.x = Mathf.Clamp(mousePosInScreenUnits, minX, maxX); // paddle'ın x koordinatı üstündeki minimum ve maksimum gideceği noktaları sınırladı
+        transform.position = paddlePos; // paddle'ın konumunu güncelledi
+    }
+}
