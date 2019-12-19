f29bc93536f18b6f6731ce6937fd11ef8db53b7d
oguzhanizam <o.nizam@outlook.com.tr>
2019-12-19 18:43:10 +0300

    blokz

--- blockz/Assets/Scripts/Block/Block.cs
+++ blockz/Assets/Scripts/Block/Block.cs
@@ -6,6 +6,26 @@ public class Block : MonoBehaviour
 {
     [SerializeField] AudioClip breakSound;
 
+    //cached reference
+    Level level;
+
+    public void Start()
+    {
+        level = FindObjectOfType<Level>();
+        level.CountBreakableBlocks();
+    }
+
+    private void OnCollisionEnter2D( Collision2D collision )
+    {
+        DestroyBlock( );
+    }
+
+    private void DestroyBlock()
+    {
+        Destroy(gameObject);
+        level.BlockDestroyed( );
+    }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         AudioSource.PlayClipAtPoint(breakSound, Camera.main.transform.position);
